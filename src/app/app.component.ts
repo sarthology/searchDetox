6fdbbbc1aa85d077d9de4f4488a561094bbe06e4
Sarthak Sharma <7lovesharma7@gmail.com>
2018-07-06 16:41:00 +0530

    Intial commit with basic functionality ready 😎

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  data;
+  currentKeywords;
+  currentKeywordIndex:number = 0;
+  timer;
+  choosenPersonality;
+
+
+  constructor(private http:HttpClient){
+    this.http.get('assets/data.json').subscribe(res=>{
+      this.data = res; 
+    })
+  }
+
+  destroyGoogle(){
+    this.currentKeywords = this.data.personalities[this.choosenPersonality]; 
+    this.timer = setInterval(()=>{
+      //Open window with new keyword
+      this.openWindow();
+    }, 5000);
+  }
+  
+  //Open new tab with keywords
+  openWindow(){
+    if(this.currentKeywords[this.currentKeywordIndex]){
+      window.open("http://google.com/search?q="+this.currentKeywords[this.currentKeywordIndex])
+      this.currentKeywordIndex += 1;
+    }
+    else{
+      this.currentKeywordIndex = 0;
+      clearInterval(this.timer);
+    }
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+// this is sparta
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
